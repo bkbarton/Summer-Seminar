70be27a05c38f576deba32052f8328bbf9ecebfb
Kyle Barton <bk.barton@yahoo.com>
2015-06-27 17:28:45 -0500

    Items
    Added some items into the game along with some scripts associated to
    them.

---
+++ Group Project/Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item {
+	public string 		itemName;
+	public int 			itemID;
+	public string		itenDesc;
+	public Texture2D	itemIcon;
+	public int			itemPower;
+	public int			itemSpeed;
+	public ItemType		itemType;
+
+	public enum ItemType{
+		Weapon,
+		Armour,
+		Consumable
+	}
+}

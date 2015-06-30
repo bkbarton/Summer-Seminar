49628c5cc2c0aaea0050b3821cc389ee22ec390a
Patrick Geiger <patrickjgeiger@gmail.com>
2015-06-30 03:15:02 -0400

    Combat
    Combat system started. State machine and chance to encounter an enemy
    while walking through the dungeon is implemented.

--- Group Project/Assets/Scripts/PlayerMovement.cs
+++ Group Project/Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,60 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed = 0.4f;
-	
+    public static bool canRunIntoEnemy = true; //true for testing, normally would start at false 
+    public float enemyChance = 50;
+
+
+
 	Vector2 dest = Vector2.zero;
 	// Use this for initialization
 	void Start () {
 		dest = transform.position;
-		
 	}
 	
 	
 	void FixedUpdate () {
 		Vector2 p = Vector2.MoveTowards (transform.position, dest, speed); 
 		GetComponent<Rigidbody2D>().MovePosition (p);
-		
+		float ind = Random.Range(0,200);
+
 		//movement
-		
-		if (Input.GetKey(KeyCode.UpArrow)&& valid(Vector2.up))
-			dest = (Vector2)transform.position + Vector2.up;
-		if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
-			dest = (Vector2)transform.position + Vector2.right;
-		if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up) )
-			dest = (Vector2)transform.position - Vector2.up;
-		if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
-			dest = (Vector2)transform.position - Vector2.right;
+        //enemy chance is also tied to movement, as the player moves they have a chance to encounter an enemy
+        //the chance is deteremined by enemyChance, if the random number generator gives a number below EnemyChance then an encounter begins
+
+        if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+        {
+            dest = (Vector2)transform.position + Vector2.up;
+            if (ind < enemyChance)
+            {
+                TurnBasedCombat.S.SetState(CombatStates.START);
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+        {
+            dest = (Vector2)transform.position + Vector2.right;
+            if (ind < enemyChance)
+            {
+                print(ind);
+                TurnBasedCombat.S.SetState(CombatStates.START);
+            }
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+        { 
+            dest = (Vector2)transform.position - Vector2.up;
+            if (ind < enemyChance)
+            {
+                TurnBasedCombat.S.SetState(CombatStates.START);
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+        {
+            dest = (Vector2)transform.position - Vector2.right;
+            if (ind < enemyChance)
+            {
+                TurnBasedCombat.S.SetState(CombatStates.START);
+            }
+        }
 		
 		//Animation
 		Vector2 dir = dest - (Vector2)transform.position;
@@ -34,6 +65,7 @@ public class PlayerMovement : MonoBehaviour {
 		GetComponent<Animator>().SetFloat ("DirY", dir.y);
 		
 		
+
 	}
 	
 	bool valid(Vector2 dir){
@@ -42,4 +74,5 @@ public class PlayerMovement : MonoBehaviour {
 		RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
 		return (hit.collider == GetComponent<Collider2D>()) ;
 	}
+
 }
